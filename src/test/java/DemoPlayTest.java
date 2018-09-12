9779e91f02912d8aa60d8d3a0a7476e5c75b1720
Daneel Yaitskov <dyaitskov@gmail.com>
2018-09-11 22:50:33 -0700

    working GET echo

---
+++ src/test/java/DemoPlayTest.java
@@ -0,0 +1,21 @@
+import static play.mvc.Results.ok;
+import static play.routing.RoutingDsl.fromComponents;
+import static play.server.Server.forRouter;
+
+import org.junit.Test;
+import play.server.Server;
+
+public class DemoPlayTest {
+    @Test
+    public void run() throws InterruptedException {
+        Server server = forRouter(
+                9001,
+                (components) ->
+                        fromComponents(components)
+                                .GET("/hello/:to")
+                                .routeTo(to -> ok("Cześć [" + to + "]"))
+                                .build());
+        Thread.sleep(1111111);
+    }
+
+}
